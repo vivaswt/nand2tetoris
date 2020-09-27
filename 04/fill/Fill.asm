e757a25dfc4632a9806901afe88c47740f317c67
vivaswt <vivaswt@gmail.com>
2020-09-27 20:57:16 +0900

    chap4 mult is complete

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,55 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    // if KBD > 0 then
+    //   flag = -1
+    //   goto DRAW
+    // else
+    //   flag = 0
+    @KBD
+    D=M
+    @PRESSED
+    D;JGT
+    @flag
+    M=0
+    @DRAW
+    0;JMP
+
+(PRESSED)
+    @flag
+    M=-1
+
+(DRAW)
+    // At this time, flag is set as value to update screen.
+    @i
+    M=0
+
+(DRAW_LOOP)
+    // calcuate address in screen.
+    // position = SCREEN + i
+    @SCREEN
+    D=M
+    @i
+    D=D+A
+    @position
+    M=D
+
+    // update screen
+    @flag
+    D=M
+    @position
+    M=D
+
+    // i = i + 1
+    // if (i > 8192) goto LOOP
+    @i
+    M=M+1
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JGT
+
+    @DRWA_LOOP
+    0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,36 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    // counter = R1
+    @R1
+    D=M
+    @counter
+    M=D
+
+    // R2 = 0
+    @R2
+    M=0
+
+(LOOP)
+    // counter = counter - 1
+    // if (counter < 0) goto END
+    @counter
+    MD=M-1
+    @END
+    D;JLT
+
+    // R2 = R2 + R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(END)
+
+(INFINITE_LOOP)
+    @INFINITE_LOOP
+    0;JMP
