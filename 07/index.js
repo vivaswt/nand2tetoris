d7e9da8d06584c5d0a1a51d1ca369de47d61f48c
vivaswt <vivaswt@gmail.com>
2020-10-25 15:44:51 +0900

    chap7 eqまで実装

--- 07/index.js
+++ 07/index.js
@@ -35,48 +35,62 @@ class Parser extends Transform {
         super(_options);
     }
 
+    eraseComment(chunk) {
+        let pos = chunk.indexOf('//');
+        if (pos > -1) {
+            return chunk.substr(0, pos);
+        } else {
+            return chunk;
+        }
+    }
+
+    parseAsCommand(chunk) {
+        const words = chunk.split(/\s/).filter(w => w.length > 0);
+        if (words.length === 0 || words.length > 3) return null;
+
+        const result = {
+            type: '?',
+            arg1: '',
+            arg2: ''
+        };
+
+        switch (words[0]) {
+            case 'add':
+            case 'sub':
+            case 'neg':
+            case 'eq':
+            case 'gt':
+            case 'lt':
+            case 'and':
+            case 'or':
+            case 'not':
+                result.type = 'ARITHMETIC';
+                result.arg1 = words[0];
+                break;
+        
+            case 'push':
+                result.type = 'PUSH';
+                result.arg1 = words[1];
+                result.arg2 = parseInt(words[2]);
+                break;
+
+            case 'pop':
+                result.type = 'POP';
+                result.arg1 = words[1];
+                result.arg2 = parseInt(words[2]);
+                break;
+
+            default:
+                break;
+        }
+        return result;
+    }
+
     _transform(chunk, encoding, callback) {
-        const result = {};
-
-        result.type = '?';
-
-        // 算術論理コマンド
-        const acomExp = /^\s*(add|sub|neg|eq|gt|lt|and|or|not)\s*(\/\/.*)?$/;
-        const acomResult = acomExp.exec(chunk);
-        if (acomResult !== null) {
-            result.type = 'ARITHMETIC';
-            result.arg1 = acomResult[1];
-        }
-
-        // PUSHコマンド判定
-        const pushExp = /^\s*push\s+(\w+)\s+(\w+)\s*(\/\/.*)?$/;
-        const pushResult = pushExp.exec(chunk);
-        if (pushResult !== null) {
-            result.type = 'PUSH';
-            result.arg1 = pushResult[1];
-            result.arg2 = parseInt(pushResult[2]);
-        }
-
-        // POPコマンド判定
-        const popExp = /^\s*pop\s+(\w+)\s+(\w+)\s*(\/\/.*)?$/;
-        const popResult = popExp.exec(chunk);
-        if (popResult !== null) {
-            result.type = 'POP';
-            result.arg1 = popResult[1];
-            result.arg2 = parseInt(popResult[2]);
-        }
-
-        // 空行・コメント行判定
-        const commentExp = /^\s*(\/\/(.*))?$/;
-        const commentResult = commentExp.exec(chunk);
-        if (commentResult !== null) {
-            result.type = 'COMMENT';
-            result.comment = this.unstr(commentResult[2]);
-        }
-
-        result.text = chunk;
-
-        if (result.type !== 'COMMENT') {
+        const line = this.eraseComment(chunk);
+        const result = this.parseAsCommand(line);
+        if (result) {
+            result.text = chunk;
             this.push(result);
         }
         callback();
@@ -94,21 +108,144 @@ class Translator extends Transform {
         _options.readableObjectMode = true;
         _options.writableObjectMode = true;
         super(_options);
+
+        this.labelNumber = 1;
+    }
+
+    translatePUSH(command) {
+        if (command.arg1 !== 'constant') [];
+        const result = [];
+        result.push(`// ** push constant ${command.arg2} **`);
+        result.push(`@${command.arg2}`);
+        result.push(`D=A`);
+        result.push(`@SP`);
+        result.push(`A=M`);
+        result.push(`M=D`);
+        result.push(`@SP`);
+        result.push(`M=M+1`);
+        return result;
+    }
+
+    translateArithmetic(command) {
+        switch (command.arg1) {
+            case 'add':
+                return this.translateAdd(command);
+            case 'sub':
+                return this.translateSub(command);
+            case 'neg':
+                return this.translateNeg(command);
+            case 'eq':
+                return this.translateEq(command);
+            default:
+                break;
+        }
+    }
+
+    translateAdd(command) {
+        const result = [];
+        result.push(`// ** add **`);
+
+        result.push(`@SP`);
+        result.push(`M=M-1`)
+        result.push(`A=M`);
+        result.push(`D=M`);
+        result.push(`@SP`);
+        result.push(`M=M-1`)
+        result.push(`A=M`);
+        result.push(`M=D+M`)
+        result.push(`@SP`);
+        result.push(`M=M+1`);
+        return result;
+    }
+
+    translateSub(command) {
+        const result = [];
+        result.push(`// ** sub **`);
+        result.push(`@SP`);
+        result.push(`M=M-1`)
+        result.push(`A=M`);
+        result.push(`D=M`);
+        result.push(`@SP`);
+        result.push(`M=M-1`)
+        result.push(`A=M`);
+        result.push(`M=M-D`)
+        result.push(`@SP`);
+        result.push(`M=M+1`);
+        return result;
+    }
+
+    translateNeg(command) {
+        const result = [];
+        result.push(`// ** neg **`);
+        result.push(`@SP`);
+        result.push(`A=M`);
+        result.push(`A=A-1`);
+        result.push(`M=-M`);
+        return result;
+    }
+
+    translateEq(command) {
+        const result = [];
+
+        result.push(`// ** eq **`);
+        result.push(`// @SP = @SP - 1`);
+        result.push(`@SP`);
+        result.push(`M=M-1`)
+
+        result.push(`// D = *@SP`);
+        result.push(`A=M`);
+        result.push(`D=M`);
+
+        result.push(`// @SP = @SP - 1`);
+        result.push(`@SP`);
+        result.push(`M=M-1`);
+
+        result.push(`// A = *@SP`);
+        result.push(`A=M`);
+        
+        result.push(`// substract 2 values`);
+        result.push(`D=M-D`);
+        result.push(`@COMP${this.labelNumber++}`);
+        result.push(`D;JEQ`);
+
+        result.push(`// case a != b`);
+        result.push(`@SP`);
+        result.push(`A=M`);
+        result.push(`M=0`);
+        result.push(`@COMP${this.labelNumber++}`);
+        result.push(`0;JMP`);
+
+        result.push(`// case a == b`);
+        result.push(`(COMP${this.labelNumber - 2})`);
+        result.push(`D=-1`);
+        result.push(`@SP`);
+        result.push(`A=M`);
+        result.push(`M=D`);
+
+        result.push(`// @SP++`);
+        result.push(`(COMP${this.labelNumber - 1})`);
+        result.push(`@SP`);
+        result.push(`M=M+1`);
+
+        return result;
+    }
+
+    pushResults(results) {
+        results.forEach(result => {
+            this.push(result);
+        });
     }
 
     _transform(chunk, encoding, callback) {
-        let result = '';
         switch (chunk.type) {
-            case 'ARITHMETIC':
-                this.push(chunk.arg1);
-                break;
             case 'PUSH':
-                this.push(`push ${chunk.arg1} ${chunk.arg2}`)
+                this.pushResults(this.translatePUSH(chunk));
                 break;
-            case 'POP':
-                this.push(`pop ${chunk.arg1} ${chunk.arg2}`)
+            case 'ARITHMETIC':
+                this.pushResults(this.translateArithmetic(chunk));
                 break;
             default:
+                break;
         }
         callback();
     }
